42d8db2fbc6b38faf6c7851a1f593643bf6f0665
22100042 <22100042@handong.ac.kr>
2025-01-06 22:14:20 +0900

    controller 수정 (404 에러)

--- src/main/java/com/example/biblesearch/repository/BibleRepository.java
+++ src/main/java/com/example/biblesearch/repository/BibleRepository.java
@@ -9,7 +9,7 @@ public interface BibleRepository extends JpaRepository<Bible, Integer> {
 
     // sentence 컬럼에 keyword가 포함되어 있는 구절 검색 + 페이징
     Page<Bible> findBySentenceContaining(String keyword, Pageable pageable);
-
+    // 수정해야할분은 controller
     // 전체 목록 + 페이징
     // (사실 JpaRepository 자체에 findAll(Pageable pageable) 있음)
     // Page<Bible> findAll(Pageable pageable); // 이미 상속받은 메서드라 생략 가능
